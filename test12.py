a08b8ce741ca228a54dd9cfc4c3a862b1a477bbe
haozj <594796355@qq.com>
2019-06-19 23:58:53 +0800

    test10

---
+++ test12.py
@@ -0,0 +1,18 @@
+# encoding:utf-8
+# author:haozj 
+# create_time: 2019/6/19
+from math import sqrt
+
+count = 0
+flag = 1
+for i in range(101, 201):
+    mid = int(sqrt(i + 1))
+    for j in range(2, mid+1):
+        if (i % j == 0):
+            flag = 0
+            break
+    if flag == 1:
+        print(i)
+        count += 1
+    flag = 1
+print(count)
